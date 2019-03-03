1cc8434386bbee760b9f78e488ab34f26e89b25a
lidek <lidia.stopinska@gmail.com>
2019-03-03 21:30:50 +0000

    finished homework

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -5,18 +5,17 @@ end
 def total_cash(pet_shop)
  return pet_shop[:admin][:total_cash]
 end
+# for some reason does not work
+# def add_or_remove_cash(pet_shop, add_cash)
+#   total_cash(pet_shop) += add_cash
+# end
 
 def add_or_remove_cash(pet_shop, add_cash)
  pet_shop[:admin][:total_cash] += add_cash
 end
 
-def add_or_remove_cash__remove(pet_shop, remove_cash)
- pet_shop[:admin][:total_cash] -= add_cash
-end
-
 def pets_sold(pet_shop)
   return pet_shop[:admin][:pets_sold]
-#   assert_equal(0, sold)
 end
 
 def increase_pets_sold(pet_shop, num)
@@ -28,20 +27,24 @@ def stock_count(pet_shop)
   for pets in pet_shop[:pets]
     pets_total += 1
    end
-   puts pet_shop[:pets].count # can use this as return rather than for loop
+   # puts pet_shop[:pets].count # can use this as return rather than for loop
    return pets_total
 end
 
 # test 8 and  9
 def pets_by_breed(pet_shop, breed)
-   types = []
+    pets_breed = []
   for pet in pet_shop[:pets]
     if pet[:breed] == breed
-      types.push(breed)
+      pets_breed.push(pet)
      end
-  end
-  return types
-end
+    end
+  #    print pets_breed
+  #    pets_breed = [{:name=>"Sir Percy", :pet_type=>:cat, :breed=>"British Shorthair", :price=>500}, {:name=>"King Bagdemagus",
+  #    :pet_type=>:cat, :breed=>"British Shorthair", :price=>500}]
+    return pets_breed
+ end
+
 # test 10 & 11
 def find_pet_by_name(pet_shop, call)
   for pet in pet_shop[:pets]
@@ -85,26 +88,26 @@ end
 
 
     def add_pet_to_customer(cust, call)
-         puts cust
+         # puts cust
          cust[:pets] << call
-         puts cust
-         puts cust[:pets].length
+         # puts cust
+         # puts cust[:pets].length
      end
 
 
        def customer_can_afford_pet(cust, call)
          if cust[:cash] >= call[:price]
            return true
-         end
-       else
+        else
          false
        end
+      end
 
        # #These are 'integration' tests so we want multiple asserts.
        # #If one fails the entire test should fail
 
 
-       # def find_pet_by_name(pet_shop, call)
+       # def find_pet_by_name(pet_shop, call) copy of method for refence
        #   for pet in pet_shop[:pets]
        #     if pet[:name] == call
        #       return pet
@@ -118,18 +121,16 @@ end
 
        def sell_pet_to_customer(pet_shop, call, cust)
          if call == nil
-          puts "No pet was passed in"
+          # puts "No pet was passed in"
           return
         end
          customer_can_afford = customer_can_afford_pet(cust, call)
          customer_pets = customer_pet_count(cust)
-         if call != nil && customer_can_afford
-           puts " customer can afford and pet exists"
+         if customer_can_afford
+           # puts " customer can afford and pet exists"
            add_pet_to_customer(cust, call)
            increase_pets_sold(pet_shop, 1)
            remove_customer_cash(cust, call[:price])
            add_or_remove_cash(pet_shop, call[:price])
           end
        end
-
-      
